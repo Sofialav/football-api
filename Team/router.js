6c77c1f902d7a57d0143bfd47f8f7a095dd4f5d1
Sofialav <cemeteryvixen@gmail.com>
2020-02-26 17:29:20 +0100

    minor adjs

--- Team/router.js
+++ Team/router.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const Team = require("./model");
 const router = new Router();
 
-router.get("/teams", (req, res, next) => {
+router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teams => {
       if (teams) {
