99125c93b47e27b6bd85df6845cfbd3fc6559198
Sofialav <cemeteryvixen@gmail.com>
2020-02-26 18:01:10 +0100

    /team => /teams

--- Team/router.js
+++ Team/router.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const Team = require("./model");
 const router = new Router();
 
-router.get("/team", (req, res, next) => {
+router.get("/teams", (req, res, next) => {
   Team.findAll()
     .then(teams => {
       if (teams) {
@@ -14,19 +14,19 @@ router.get("/team", (req, res, next) => {
     .catch(next);
 });
 
-router.post("/team", (req, res, next) => {
+router.post("/teams", (req, res, next) => {
   Team.create(req.body)
     .then(team => res.status(201).json(team))
     .catch(next);
 });
 
-router.get("/team/:teamId", (req, res, next) => {
+router.get("/teams/:teamId", (req, res, next) => {
   Team.findByPk(req.params.teamId)
     .then(team => {
       if (team) {
         return res.status(200).json(team);
       } else {
-        res.status(404).send("Tean does not exist");
+        res.status(404).send("Team does not exist");
       }
     })
     .catch(next);
