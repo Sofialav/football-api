ecb0238f974ac27e43464000bf3f325585b1e131
Sofialav <cemeteryvixen@gmail.com>
2020-02-26 17:49:11 +0100

    team post endpoint

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const teamRouter = require("./Team/router");
 
 const app = express();
 const port = process.env.PORT || 4000;
+app.use(bodyParser.json());
 app.use(teamRouter);
 
 app.listen(port, () => console.log("Listening to " + port));
