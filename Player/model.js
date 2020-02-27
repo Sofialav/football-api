41bc3aad548bae363c643e4c3edbf1833955062a
Sofialav <cemeteryvixen@gmail.com>
2020-02-27 11:09:45 +0100

    Team-Player relation added

--- Player/model.js
+++ Player/model.js
@@ -1,9 +1,11 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const Team = require("../Team/model");
 
 const Player = db.define("player", {
   name: { type: Sequelize.STRING, allowNull: false },
   number: { type: Sequelize.INTEGER }
 });
+Player.belongsTo(Team);
 
 module.exports = Player;
